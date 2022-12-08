8fa98ecbd55e2f416afd5811725ef0a770a965e4
Olaphoxy <horlaps96@gmail.com>
2022-12-08 18:00:09 +0000

    new

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -14,7 +14,7 @@ const Searchbar = () => {
     <form
       onSubmit={handleSubmit}
       autoComplete="off"
-      className="p-2 text-gray-400 focus-within:text-gray-600 fixed"
+      className="p-2 text-gray-400 focus-within:text-gray-600 "
     >
       <label htmlFor="search-field" className="sr-only">
         Search all songs
